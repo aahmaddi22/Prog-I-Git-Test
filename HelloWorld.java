a7f523a19fb70e6340bc00c84b56c8e3220c4d02
Ali Ahmad <aliahmaddin21@gmail.com>
2025-09-23 11:58:30 +0000

    Segon commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 cat HelloWorld.java
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello, world!");
+		System.out.println("Hello, world of Git!");
 	}
 }
